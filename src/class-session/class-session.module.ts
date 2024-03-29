cea3971d473b764a6eeb207afa4801d2b895a2e0
Lephat902 <lehoangngocphat992002@gmail.com>
2024-03-29 18:08:12 +0700

    Resolve class, tutor, student for class session

--- src/class-session/class-session.module.ts
+++ src/class-session/class-session.module.ts
@@ -7,6 +7,7 @@ import { QueueNames } from '@tutorify/shared';
 import { ClassModule } from 'src/class/class.module';
 import { Resolvers } from './resolvers';
 import { AddressModule } from 'src/address/address.module';
+import { TutorQueryModule } from 'src/tutor-query/tutor-query.module';
 
 @Module({
   imports: [
@@ -28,6 +29,7 @@ import { AddressModule } from 'src/address/address.module';
     ]),
     forwardRef(() => ClassModule),
     AddressModule,
+    TutorQueryModule,
   ],
   controllers: [ClassSessionController],
   providers: [ClassSessionService, ...Resolvers],
