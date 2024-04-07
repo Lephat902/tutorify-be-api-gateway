fbf242b200d9973fa3ced43a95042fe10dba0441
Lephat902 <lehoangngocphat992002@gmail.com>
2024-04-07 18:14:58 +0700

    GraphQL: Add classCategory endpoint

--- src/class-category/resolvers/class-category.resolver.ts
+++ src/class-category/resolvers/class-category.resolver.ts
@@ -7,6 +7,11 @@ import { ClassCategoryQueryArgs } from '../args';
 export class ClassCategoryResolver {
   constructor(private readonly classCategoryService: ClassCategoryService) {}
 
+  @Query(() => ClassCategory, { name: 'classCategory' })
+  async getClassCategoryById(@Args('id') id: string): Promise<ClassCategory> {
+    return this.classCategoryService.findById(id);
+  }
+
   @Query(() => [ClassCategory], { name: 'classCategories' })
   async getAllClassCategories(@Args() classCategoryQueryArgs: ClassCategoryQueryArgs): Promise<ClassCategory[]> {
     return this.classCategoryService.findAll(classCategoryQueryArgs);
