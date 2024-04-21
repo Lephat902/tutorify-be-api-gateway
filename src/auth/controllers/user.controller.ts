c51123b45bfd5c96849569a73dc63e40446fa767
Lephat902 <lehoangngocphat992002@gmail.com>
2024-04-21 08:12:57 +0700

    Refactor auth controllers

--- src/auth/controllers/user.controller.ts
+++ src/auth/controllers/user.controller.ts
@@ -1,13 +1,9 @@
 import {
   Body,
-  Query,
   Controller,
-  Get,
-  Post,
   UseGuards,
   Param,
   Patch,
-  BadRequestException,
   Delete,
 } from '@nestjs/common';
 import {
@@ -15,50 +11,18 @@ import {
   ApiOperation,
   ApiTags,
 } from '@nestjs/swagger';
-import { AuthService } from './auth.service';
-import { TokenGuard } from './guards';
-import { Token, TokenRequirements } from './decorators';
-import { IAccessToken, TokenType } from './auth.interfaces';
-import {
-  LoginDto,
-  SignUpDto,
-  SignUpStudentDto,
-  SignUpTutorDto,
-  VerifyEmailDto,
-} from './dtos';
+import { AuthService } from '../auth.service';
+import { TokenGuard } from '../guards';
+import { Token, TokenRequirements } from '../decorators';
+import { IAccessToken, TokenType } from '../auth.interfaces';
 import { UserRole } from '@tutorify/shared';
-import { UpdateDto, UpdateStudentDto, UpdateTutorDto } from './dtos/update';
+import { UpdateDto, UpdateStudentDto, UpdateTutorDto } from '../dtos/update';
 
 @Controller()
 @ApiTags('authentication')
 @UseGuards(TokenGuard)
-export class AuthController {
+export class UserController {
   constructor(private readonly authService: AuthService) { }
-
-  @Get('profile')
-  @ApiOperation({ summary: 'Get self information' })
-  @ApiBearerAuth()
-  @TokenRequirements(TokenType.CLIENT, [])
-  public async getSelf(@Token() token: IAccessToken) {
-    return this.authService.getUserById(token.id);
-  }
-
-  @Get('token/user/:id')
-  @TokenRequirements(TokenType.SYSTEM, [])
-  @ApiOperation({ summary: 'Get access token by user id' })
-  @ApiBearerAuth()
-  public async getTokenByUserId(@Param('id') id: string) {
-    const user = await this.authService.getUserById(id);
-    return this.returnUserAndToken(user);
-  }
-
-  @Get('confirm-email')
-  @ApiOperation({
-    summary: 'User click this URL in email received to verify their email',
-  })
-  public async verifyEmail(@Query() verifyEmailDto: VerifyEmailDto) {
-    return this.authService.verifyEmail(verifyEmailDto.token);
-  }
 
   @Patch('users/:userId/password/reset')
   @TokenRequirements(TokenType.CLIENT, [UserRole.ADMIN])
@@ -68,44 +32,6 @@ export class AuthController {
   @ApiBearerAuth()
   public async resetPasswordByAdmin(@Param('userId') userId: string) {
     return this.authService.resetPasswordByAdmin(userId);
-  }
-
-  @Post('tutors/signup')
-  @ApiOperation({ summary: 'Sign up a new tutor.' })
-  public async signUpTutor(
-    @Body() signupTutorDto: SignUpTutorDto,
-  ) {
-    signupTutorDto.role = UserRole.TUTOR;
-    return this.authService.createUser(signupTutorDto);
-  }
-
-  @Post('students/signup')
-  @ApiOperation({ summary: 'Sign up a new student.' })
-  public async signUpStudent(
-    @Body() signUpStudentDto: SignUpStudentDto,
-  ) {
-    signUpStudentDto.role = UserRole.STUDENT;
-    return this.authService.createUser(signUpStudentDto);
-  }
-
-  @Post('managers/signup')
-  @ApiOperation({ summary: 'Admin creates new manager.' })
-  @TokenRequirements(TokenType.CLIENT, [UserRole.ADMIN])
-  @ApiBearerAuth()
-  public async signUpManager(
-    @Body() signUpManagerDto: SignUpDto,
-  ) {
-    signUpManagerDto.role = UserRole.MANAGER;
-    return this.authService.createUser(signUpManagerDto);
-  }
-
-  @Post('login')
-  @ApiOperation({ summary: 'Sign in user via email/username and password' })
-  public async login(@Body() loginDto: LoginDto) {
-    if (!loginDto.username && !loginDto.email)
-      throw new BadRequestException("Provide either email or username to log in");
-    const user = await this.authService.login(loginDto);
-    return this.returnUserAndToken(user);
   }
 
   @Patch('tutors/profile')
@@ -195,12 +121,5 @@ export class AuthController {
   @ApiBearerAuth()
   public async deleteUserByEmail(@Param('email') email: string) {
     return this.authService.deleteUser({ email });
-  }
-
-  private returnUserAndToken(user: any) {
-    return {
-      user,
-      token: this.authService.createAccessTokenFromAuthUser(user),
-    };
   }
 }
