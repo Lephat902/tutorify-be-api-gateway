b0c676bdce1b47b67dfbfe5e2f9ea9ac890f5772
Lephat902 <lehoangngocphat992002@gmail.com>
2024-04-29 19:18:04 +0700

    Only allow admin and manager to query users

--- src/auth/resolvers/user-paginated-results.resolver.ts
+++ src/auth/resolvers/user-paginated-results.resolver.ts
@@ -2,13 +2,20 @@ import { Resolver, Query, Args } from '@nestjs/graphql';
 import { AuthService } from '../auth.service';
 import { UserQueryArgs } from '../args';
 import { UserPaginatedResults } from '../models/user-paginated-results.model';
+import { TokenGuard } from '../guards';
+import { UseGuards } from '@nestjs/common';
+import { TokenType } from '../auth.interfaces';
+import { UserRole } from '@tutorify/shared';
+import { TokenRequirements } from '../decorators';
 
 @Resolver(() => UserPaginatedResults)
+@UseGuards(TokenGuard)
 export class UserPaginatedResultsResolver {
   constructor(
     private readonly authService: AuthService,
   ) { }
 
+  @TokenRequirements(TokenType.CLIENT, [UserRole.MANAGER, UserRole.ADMIN])
   @Query(() => UserPaginatedResults, { name: 'users' })
   async getUsersAndTotalCount(
     @Args() filters: UserQueryArgs,
